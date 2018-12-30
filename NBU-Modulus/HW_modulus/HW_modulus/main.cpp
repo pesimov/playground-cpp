4cb4dea9bb8e74bfcb6a94a58ca272c810f9e4ed
Peter Simov <peter.simov@gmail.com>
2018-12-30 11:49:42 +0200

    Commit all files

---
+++ NBU-Modulus/HW_modulus/HW_modulus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW_modulus
+//
+//  Created by Peter Simov on 30.12.18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
